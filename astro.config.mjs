7579b994547d925ac5e837162640a6434c64d6d5
troy-endource <troy@endource.com>
2026-04-09 14:54:54 +0100

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://thedressedit.co.uk',
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: { theme: 'css-variables' }
   }
